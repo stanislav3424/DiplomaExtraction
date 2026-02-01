80aa825458220be1c5586f65f9a0c0b1e29a7f3f
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-01 22:11:17 +0300

    Add WidgetLogic

---
+++ Source/DiplomaExtraction/ExfilHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "ExfilHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API AExfilHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DiplomaExtraction/UW_MainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UW_Dialog.h"
+#include "UW_MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API UUW_MainMenu : public UUW_Dialog
+{
+	GENERATED_BODY()
+	
+};

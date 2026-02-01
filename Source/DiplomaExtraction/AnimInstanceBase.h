7b1ede16e4abd6400423a682d05289426fb6145b
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-01 17:59:34 +0300

    Add IconRendering, AnimInstanse, Animation, GameCharacter

---
+++ Source/DiplomaExtraction/AnimInstanceBase.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AnimInstanceBase.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class DIPLOMAEXTRACTION_API UAnimInstanceBase : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DiplomaExtraction/BP_Actor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActorBase.h"
+#include "BP_Actor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API ABP_Actor : public AActorBase
+{
+	GENERATED_BODY()
+	
+};

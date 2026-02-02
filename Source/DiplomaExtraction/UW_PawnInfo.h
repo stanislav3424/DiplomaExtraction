964464135c85b77f7b18e599d97a0d4906a64a14
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-02 17:10:09 +0300

    working on inventory

---
+++ Source/DiplomaExtraction/UW_PawnInfo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UW_Dialog.h"
+#include "UW_PawnInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API UUW_PawnInfo : public UUW_Dialog
+{
+	GENERATED_BODY()
+	
+};

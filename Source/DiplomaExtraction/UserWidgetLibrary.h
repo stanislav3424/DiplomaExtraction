a9eb7212a9f457c37fe308ca5713ad5c96dc4b10
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-03 19:10:22 +0300

    Setting IconRendering Add minimal UW and M UW

---
+++ Source/DiplomaExtraction/UserWidgetLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UserWidgetLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API UUserWidgetLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};

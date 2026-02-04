d86d2b9fd3705910ba14cce5f986a7cdc7d44207
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-04 09:52:43 +0300

    working on PawnInfo

---
+++ Source/DiplomaExtraction/UW_Equipment.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UW_Base.h"
+#include "UW_Equipment.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class DIPLOMAEXTRACTION_API UUW_Equipment : public UUW_Base
+{
+	GENERATED_BODY()
+	
+};

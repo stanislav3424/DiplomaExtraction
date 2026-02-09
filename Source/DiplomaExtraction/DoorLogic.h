c0dd6e15a5d12ad0b4b3f1549ffa878a780ce6d2
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-09 17:05:54 +0300

    add RoorLogic

---
+++ Source/DiplomaExtraction/DoorLogic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LogicBase.h"
+#include "DoorLogic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API UDoorLogic : public ULogicBase
+{
+	GENERATED_BODY()
+	
+};

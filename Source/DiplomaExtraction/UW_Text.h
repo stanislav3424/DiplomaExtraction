ab0bbf87bf2d0a25f9177aa5bc2fe16f8d15591d
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-10 12:02:48 +0300

    automatic application of quest items

---
+++ Source/DiplomaExtraction/UW_Text.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UW_Base.h"
+#include "UW_Text.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API UUW_Text : public UUW_Base
+{
+	GENERATED_BODY()
+	
+};

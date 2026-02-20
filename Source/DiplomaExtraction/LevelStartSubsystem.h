db7836ccde49009be096ad16f569ae30b81ff6b1
Stanislav <dudoladov_stanislav@mail.ru>
2026-02-20 18:09:11 +0300

    Work on the logic of the beginning/end of the game and the global interface

---
+++ Source/DiplomaExtraction/LevelStartSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "LevelStartSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMAEXTRACTION_API ULevelStartSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
